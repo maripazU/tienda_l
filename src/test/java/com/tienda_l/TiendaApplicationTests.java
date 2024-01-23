463d14a01b471807a0f677e174755130bf33334c
scara14 <144374635+scara14@users.noreply.github.com>
2024-01-22 20:49:05 -0600

    Semana 2: css

--- src/test/java/com/tienda_l/TiendaApplicationTests.java
+++ src/test/java/com/tienda_l/TiendaApplicationTests.java
@@ -3,6 +3,8 @@ package com.tienda_l;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//si algo comienza a dar errores entonces hay que borrar esta clase y crearla de nuevo
+
 @SpringBootTest
 class TiendaApplicationTests {
 
